b3001a877c787d3e2c294b5d75cd9f74853e6a0d
Gutu Anatolie (SI-221) <gutu.anatolie1000.7@gmail.com>
2024-02-17 13:53:46 +0200

    previous commit was a test commit

--- FirstProj/Global.asax.cs
+++ FirstProj/Global.asax.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -16,6 +16,8 @@ namespace FirstProj
             // Code that runs on application startup
            AreaRegistration.RegisterAllAreas();
            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+           RouteConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
