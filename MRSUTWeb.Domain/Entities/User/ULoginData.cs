dc45a547f53cbda92ba8b05b800039bf02bed8bf
Gutu Anatolie (SI-221) <gutu.anatolie1000.7@gmail.com>
2024-03-10 20:20:32 +0200

    Added ULoginData with classes UserLogin and ULoginData

---
+++ MRSUTWeb.Domain/Entities/User/ULoginData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRSUTWeb.Domain.Entities.User
+{
+    public class UserLogin
+    {
+        public string Credential { get; set; }
+        public string Password { get; set; }
+        public bool Status { get; set; }
+        public string StatusMsg { get; set; }
+    }
+    public class ULoginData
+    {
+        public string Credential { get; set; }
+        public string Password { get; set; }
+        public string LoginIp { get; set; }
+        public DateTime LoginDateTime { get; set; }
+    }
+    
+}
