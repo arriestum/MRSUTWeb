885b171a233261a176abed6a3a69238e89411f13
Gutu Anatolie (SI-221) <gutu.anatolie1000.7@gmail.com>
2024-03-10 20:19:56 +0200

    added SessionBL

---
+++ MRSUTWeb.BusinessLogic/SessionBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MRSUTWeb.BusinessLogic.Core;
+using MRSUTWeb.BusinessLogic.Interfaces;
+using MRSUTWeb.Domain.Entities.User;
+
+namespace MRSUTWeb.BusinessLogic
+{
+    public class SessionBL : UserApi, ISession
+    {
+        public UserLogin UserLogin(ULoginData data)
+        {
+            return UserLogin(data);
+        }
+    }
+}
