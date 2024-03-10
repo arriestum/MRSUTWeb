6c6487b174cfb3a9ac59d336110bd6ae18649097
Gutu Anatolie (SI-221) <gutu.anatolie1000.7@gmail.com>
2024-03-10 20:18:52 +0200

    added ISession and call method that return message if log in was succeseful or not

---
+++ MRSUTWeb.BusinessLogic/Interfaces/ISession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MRSUTWeb.Domain.Entities.User;
+
+namespace MRSUTWeb.BusinessLogic.Interfaces
+{
+    public interface ISession
+    {
+        UserLogin UserLogin(ULoginData data);
+    }
+
+}
