23601524b4227b069c78cceda159ce6132a88c17
Warship55 <lungudumitru55@gmail.com>
2024-03-26 14:35:33 +0200

    add dbmodel

---
+++ MRSUTWeb.BusinessLogic/DBModel/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRSUTWeb.BusinessLogic.DBModel
+{
+    class UserContext : DbContext
+    {
+        public UserContext() : base("name=UserContext")//string conectare la baza de date in web.config
+        {
+        }
+
+        public DbSet<UDbTable> Users { get; set; }
+    }
+}
