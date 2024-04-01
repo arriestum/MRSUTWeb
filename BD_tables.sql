10b36a22771a439c193267d0bcb4b34b1687e2c9
Gutu Anatolie (SI-221) <gutu.anatolie1000.7@gmail.com>
2024-04-01 14:49:37 +0300

    added relations between tables

--- BD_tables.sql
+++ BD_tables.sql
@@ -12,58 +12,61 @@ GO
 USE MRSUTWeb
 GO
 
-CREATE TABLE [dbo].[User](
-[Id]		[int] IDENTITY(1,1) NOT NULL,
+CREATE TABLE [dbo].[Userr](
+[ID_User]		[int] PRIMARY KEY,
 [Username]	[nvarchar](50) NOT NULL,
 [Password]	[nvarchar](50) NOT NULL,
 [Email]		[nvarchar](50) NOT NULL,
 [LastLogin]	[datetime] NOT NULL,
 [LastIp]	[nvarchar](30) NOT NULL,
-CONSTRAINT	[PK_UDbTables] PRIMARY KEY CLUSTERED 
-([Id] ASC)
-);
+
+)
 
 GO 
+CREATE TABLE [dbo].[Tip_tranzactie](
+[ID_tip_tranzactie] [int] PRIMARY KEY,
+[Name_transaction] 	[nvarchar](50) NOT NULL,
+);
 
-CREATE TABLE [dbo].[Card](
-[ID_Card]		[int] IDENTITY(1,1) NOT NULL,
-[Type_ID_Card]	[int] IDENTITY(1,1) NOT NULL,
-[ID_Status]		[int] IDENTITY(1,1) NOT NULL,
-[ID_User]		[int] IDENTITY(1,1) NOT NULL,
+GO
+
+CREATE TABLE [dbo].[Status_card](
+[ID_status]       [int] PRIMARY KEY,
+[Status] 		  [nvarchar](50) NOT NULL,
+);
+
+GO
+CREATE TABLE [dbo].[Tip_card](
+[Type_ID_card] [int] PRIMARY KEY,
+[Name] 		   [nvarchar](50) NOT NULL,
+);
+
+GO
+
+
+CREATE TABLE [dbo].[Cardd](
+[ID_Card]		[int] PRIMARY KEY,
+[Type_ID_Card]	[int] FOREIGN KEY REFERENCES Tip_card(Type_ID_Card),
+[ID_Status]		[int] FOREIGN KEY REFERENCES Status_card(ID_Status),
+[ID_User]		[int] FOREIGN KEY REFERENCES Userr(ID_User),
 [Number]		[char](16) NOT NULL,
 [CVV]			[char](3) NOT NULL,
 [Balance]		[float](50) NOT NULL,
 );
 
 GO 
-
 CREATE TABLE [dbo].[Transaction](
-[ID_Transaction]	      [int] IDENTITY(1,1) NOT NULL,
-[ID_card_sourse]	      [int] IDENTITY(1,1) NOT NULL,
-[Number_card_destination] [int] IDENTITY(1,1) NOT NULL,
+[ID_Transaction]	      [int] PRIMARY KEY,
+[ID_card_sourse]	      [int] FOREIGN KEY REFERENCES Cardd(ID_Card),
+[ID_tip_tranzactie]		  [int] FOREIGN KEY REFERENCES Tip_tranzactie(ID_tip_tranzactie),
+[Number]				  [char](16) NOT NULL,
 [Date]			          [datetime] NOT NULL,
 [Balance] 	              [float](50) NOT NULL,
 );
 
 GO
 
-CREATE TABLE [dbo].[Tip_card](
-[Type_ID_card] [int] IDENTITY(1,1) NOT NULL,
-[Name] 		   [nvarchar](50) NOT NULL,
-);
 
-GO 
 
-CREATE TABLE [dbo].[Status_card](
-[ID_status]       [int] IDENTITY(1,1) NOT NULL,
-[Status] 		  [nvarchar](50) NOT NULL,
-);
 
-GO
 
-CREATE TABLE [dbo].[Tip_tranzactie](
-[ID_tip_tranzactie] [int] IDENTITY(1,1) NOT NULL,
-[Name_transaction] 	[nvarchar](50) NOT NULL,
-);
-
-GO
