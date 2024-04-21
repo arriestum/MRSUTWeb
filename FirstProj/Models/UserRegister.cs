c8a22c64a51806bdf23cb5d057e7ede5555d75ac
Ciuhrii Bogdan <mr.ciuhrii@mail.ru>
2024-04-21 15:01:02 +0300

    Adaugarea in temp user, din local db.

---
+++ FirstProj/Models/UserRegister.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Web;
+
+namespace FirstProj.Models
+{
+    public class UserRegister
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+    public class UserRegisterDBContext : DbContext
+    {
+        public DbSet<UserRegister> Users { get; set; }
+    }
+}
