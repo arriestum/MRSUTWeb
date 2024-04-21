54f86fb266d91fe98fc15bdde9b1d7a813f36955
Lungu Dumitru <lungudumitru55@gmail.com>
2024-04-21 15:47:28 +0300

    insert in temp_user , localdb

--- BD_tables.sql
+++ BD_tables.sql
@@ -13,60 +13,87 @@ USE MRSUTWeb
 GO
 
 CREATE TABLE [dbo].[Userr](
-[ID_User]		[int] PRIMARY KEY,
+[ID_User]		[int] PRIMARY KEY IDENTITY,
+[Name]	[nvarchar](50) NOT NULL,
+[Surname]	[nvarchar](50) NOT NULL,
 [Username]	[nvarchar](50) NOT NULL,
 [Password]	[nvarchar](50) NOT NULL,
 [Email]		[nvarchar](50) NOT NULL,
 [LastLogin]	[datetime] NOT NULL,
-[LastIp]	[nvarchar](30) NOT NULL,
-
+[LastIp]	[nvarchar](30) NOT NULL
 )
+GO
+
+CREATE TABLE [dbo].[Temp_Userr](
+[ID_User]		[int] PRIMARY KEY IDENTITY,
+[Name]	[nvarchar](50) NOT NULL,
+[Surname]	[nvarchar](50) NOT NULL,
+[Username]	[nvarchar](50) NOT NULL,
+[Password]	[nvarchar](50) NOT NULL,
+[Email]		[nvarchar](50) NOT NULL
+)
+
 
 GO 
 CREATE TABLE [dbo].[Tip_tranzactie](
-[ID_tip_tranzactie] [int] PRIMARY KEY,
-[Name_transaction] 	[nvarchar](50) NOT NULL,
+[ID_tip_tranzactie] [int] PRIMARY KEY IDENTITY,
+[Name_transaction] 	[nvarchar](50) NOT NULL
 );
 
 GO
 
 CREATE TABLE [dbo].[Status_card](
-[ID_status]       [int] PRIMARY KEY,
+[ID_status]       [int] PRIMARY KEY IDENTITY,
 [Status] 		  [nvarchar](50) NOT NULL,
 );
 
 GO
 CREATE TABLE [dbo].[Tip_card](
-[Type_ID_card] [int] PRIMARY KEY,
-[Name] 		   [nvarchar](50) NOT NULL,
+[Type_ID_card] [int] PRIMARY KEY IDENTITY,
+[Name] 		   [nvarchar](50) NOT NULL
 );
 
 GO
 
 
 CREATE TABLE [dbo].[Cardd](
-[ID_Card]		[int] PRIMARY KEY,
+[ID_Card]		[int] PRIMARY KEY IDENTITY,
 [Type_ID_Card]	[int] FOREIGN KEY REFERENCES Tip_card(Type_ID_Card),
 [ID_Status]		[int] FOREIGN KEY REFERENCES Status_card(ID_Status),
 [ID_User]		[int] FOREIGN KEY REFERENCES Userr(ID_User),
 [Number]		[char](16) NOT NULL,
 [CVV]			[char](3) NOT NULL,
-[Balance]		[float](50) NOT NULL,
+[Balance]		[float](50) NOT NULL
 );
 
 GO 
 CREATE TABLE [dbo].[Transaction](
-[ID_Transaction]	      [int] PRIMARY KEY,
+[ID_Transaction]	      [int] PRIMARY KEY IDENTITY,
 [ID_card_sourse]	      [int] FOREIGN KEY REFERENCES Cardd(ID_Card),
 [ID_tip_tranzactie]		  [int] FOREIGN KEY REFERENCES Tip_tranzactie(ID_tip_tranzactie),
 [Number]				  [char](16) NOT NULL,
 [Date]			          [datetime] NOT NULL,
-[Balance] 	              [float](50) NOT NULL,
+[Balance] 	              [float](50) NOT NULL
 );
 
 GO
 
-
+CREATE PROCEDURE InsertTempUser
+(
+    @ID_User INT,
+    @Name NVARCHAR(50),
+    @Surname NVARCHAR(50),
+    @Username NVARCHAR(50),
+    @Password NVARCHAR(50),
+    @Email NVARCHAR(50)
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    INSERT INTO Temp_Userr (ID_User, Name, Surname, Username, Password, Email)
+    VALUES (@ID_User, @Name, @Surname, @Username, @Password, @Email);
+END;
 
 
 
---
+++ FirstProj/Controllers/RegisterController.cs
@@ -0,0 +1,63 @@
+﻿using FirstProj.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstProj.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult NewUser()
+        {
+            return View();
+        }
+
+        // POST: Register
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult NewUser(UserRegister register)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add the user to the database         (LocalDB)\MSSQLLocalDB
+
+                string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MRSUTWeb;Integrated Security=True"; 
+                string query = "INSERT INTO Temp_Userr (Name, Surname, Username, Email, Password) VALUES (@Name, @Surname, @Username, @Email, @Password)";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ID_User", register.Id);
+                        command.Parameters.AddWithValue("@Name", register.Name);
+                        command.Parameters.AddWithValue("@Surname", register.Surname);
+                        command.Parameters.AddWithValue("@Username", register.Username);
+                        command.Parameters.AddWithValue("@Email", register.Email);
+                        command.Parameters.AddWithValue("@Password", register.Password);
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                }
+                ViewBag.Message = "User added successfully!";
+
+            }
+            else
+            {
+                ViewBag.Message = "Failed to add user!";
+                return View(register);
+            }
+            return View();
+
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
